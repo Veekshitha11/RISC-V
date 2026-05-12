0d71127c0282be5c37b4a89be30ad075aa2d5b9a
veekshitha Nelluru <veekshitha.24bcs10272@sst.scaler.com>
2026-05-12 22:28:10 +0530

    fix test_normalizer

--- tests/test_normalizer.py
+++ tests/test_normalizer.py
@@ -2,34 +2,37 @@
 
 from src.normalizer import (
     normalize_extension_name,
+    normalize_tag,
 )
 
 
-def test_remove_rv_prefix():
-
-    assert (
-        normalize_extension_name(
-            "rv_zba"
-        )
-        == "zba"
-    )
+def test_rv_zba():
+    assert normalize_extension_name("rv_zba") == "Zba"
 
 
-def test_remove_rv64_prefix():
-
-    assert (
-        normalize_extension_name(
-            "rv64_zbb"
-        )
-        == "zbb"
-    )
+def test_normalize_tag_alias():
+    assert normalize_tag("rv_zba") == "Zba"
 
 
-def test_lowercase_conversion():
+def test_rv64_zbb():
+    assert normalize_extension_name("rv64_zbb") == "Zbb"
 
-    assert (
-        normalize_extension_name(
-            "Zicsr"
-        )
-        == "zicsr"
-    )
+
+def test_rv_i():
+    assert normalize_extension_name("rv_i") == "I"
+
+
+def test_rv64_i():
+    assert normalize_extension_name("rv64_i") == "RV64I"
+
+
+def test_manual_zicsr():
+    assert normalize_extension_name("Zicsr") == "Zicsr"
+
+
+def test_uppercase_z_extension():
+    assert normalize_extension_name("ZBA") == "Zba"
+
+
+def test_none_returns_empty():
+    assert normalize_extension_name(None) == ""
